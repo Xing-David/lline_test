bd2994df154c29abab4b88f36bb3554a7056301c
uxa534_name <uxa534@gmail.com>
2018-02-07 16:19:41 +0800

    if else3

--- app.py
+++ app.py
@@ -50,11 +50,14 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    #res = requests.get('http://tw.myrenta.com/search?bcg=a&t=' + event.message.text)
-	if event.message.text='id':
-		line_bot_api.reply_message(	event.reply_token, TextSendMessage(text='黑:'+event.message.text))
-        #TextSendMessage(text=res.text))
-	else	
-		line_bot_api.reply_message( event.reply_token, TextSendMessage(text='白:'+event.message.text))
+	if event.message.text='id':    
+		res = requests.get('http://tw.myrenta.com/search?bcg=a&t=' + event.message.text)
+	else
+		res = '黑:' + event.message.text
+    line_bot_api.reply_message(	event.reply_token,
+		#TextSendMessage(text='黑:'+event.message.text))
+		TextSendMessage(text=res.text))
+		
+
 if __name__ == "__main__":
     app.run()
