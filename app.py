aa8ac2f15cdff077a2a0a19227676a766ffaf313
uxa534_name <uxa534@gmail.com>
2018-02-19 08:54:53 +0800

    push 8

--- app.py
+++ app.py
@@ -26,7 +26,8 @@ app = Flask(__name__)
 ACCESS_TOKEN = os.environ.get('ACCESS_TOKEN')
 SECRET = os.environ.get('SECRET')
 
-parser = linebot.WebhookParser('ACCESS_TOKEN')
+# parser = linebot.WebhookParser('ACCESS_TOKEN')
+# events = parser.parse(body, signature)
 
 line_bot_api = LineBotApi(ACCESS_TOKEN)
 handler = WebhookHandler(SECRET)
@@ -74,7 +75,7 @@ def sum(tex ):
 	elif (tex.find('輔助'))!=-1:
 		sun = '輔助模式:\n1.補血\n2.撐防\n3.加速'
 	elif (tex.find('廣發'))!=-1:	
-		events = parser.parse(body, signature)
+		
 		for eventx in events:
 			sun = sun + '\n' +str(eventx) 
 	else:
