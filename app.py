a88c1a84d8f81e668ac2e387eef74cbc8b812206
uxa534_name <uxa534@gmail.com>
2018-02-26 15:08:11 +0800

    go 1

--- app.py
+++ app.py
@@ -58,6 +58,7 @@ def handle_message(event):
 	elif event.message.text == "13":	
 		line_bot_api.reply_message(event.reply_token,ImageSendMessage(original_content_url='https://d1dwq032kyr03c.cloudfront.net/upload/images/20180103/20107144BJM2zuA9l7.png', preview_image_url='https://d1dwq032kyr03c.cloudfront.net/upload/images/20180103/20107144BJM2zuA9l7.png'))
 	elif event.message.text == "14":
+		print("data：")
 		line_bot_api.reply_message(event.reply_token,VideoSendMessage(original_content_url='https://www.paypalobjects.com/webstatic/mktg/videos/PayPal_AustinSMB_baseline.mp4', preview_image_url='https://d1dwq032kyr03c.cloudfront.net/upload/images/20180103/20107144BJM2zuA9l7.png'))		
 	elif event.message.text == "15":		
 		line_bot_api.reply_message(event.reply_token,TextSendMessage(text=movie()))	
