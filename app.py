15254ad1928578fadfea19263c1f806a1363cc4e
uxa534_name <uxa534@gmail.com>
2018-02-26 14:48:00 +0800

    go 1

--- app.py
+++ app.py
@@ -72,18 +72,14 @@ def forShow(tex ):
 #電影
 def movie():
     target_url = 'https://movies.yahoo.com.tw/'
-    print('Start parsing movie ...')
     rs = requests.session()
     res = rs.get(target_url, verify=False)
     res.encoding = 'utf-8'
     soup = BeautifulSoup(res.text, 'html.parser')   
-    content = ""
+    content = "X"
     for index, data in enumerate(soup.select('div.movielist_info h1 a')):
         if index == 20:
-            return content
-        print("data：")
-        print(index)
-        print(data)        
+            return content        
         title = data.text
         link =  data['href']
         content += '{}\n{}\n'.format(title, link)
