d3ae10f12de5c9ec38dad823e5e078930bc76448
uxa534_name <uxa534@gmail.com>
2018-02-26 14:27:34 +0800

    go 1

--- app.py
+++ app.py
@@ -21,8 +21,16 @@ from linebot.exceptions import (
     # MessageEvent, TextMessage, TextSendMessage,
 # )
 from linebot.models import *
+import requests 
+from bs4 import BeautifulSoup
+from urllib.request import urlretrieve
+import random
 
 
+import sys
+import datetime
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials as SAC
 
 app = Flask(__name__)
 
@@ -69,22 +77,3 @@ def forShow(tex ):
 	shoow = shoow + '\nsource_type : ' + str( tex.source.type)  
 	return shoow
 	
-#電影 
-def movie():
-    target_url = 'https://movies.yahoo.com.tw/'
-    print('Start parsing movie ...')
-    rs = requests.session()
-    res = rs.get(target_url, verify=False)
-    res.encoding = 'utf-8'
-    soup = BeautifulSoup(res.text, 'html.parser')   
-    content = ""
-    for index, data in enumerate(soup.select('div.movielist_info h1 a')):
-        if index == 20:
-            return content
-        print("data：")
-        print(index)
-        print(data)        
-        title = data.text
-        link =  data['href']
-        content += '{}\n{}\n'.format(title, link)
-    return content	
